53e9077611b27224497bb881262dc59c9afbfc52
Varun Patro <varun.kumar.patro@gmail.com>
2016-11-08 22:50:52 +0800

    Fix ignored spaces in Lexer.

--- src/main/scala/lexer/LambdaLexer.scala
+++ src/main/scala/lexer/LambdaLexer.scala
@@ -5,13 +5,13 @@ import scala.util.matching.Regex
 
 object LambdaLexer extends RegexParsers {
 
-  val dot    = "\\.".r  ^^ { _ => DOT    }
-  val lambda = "\\\\".r ^^ { _ => LAMBDA }
-  val open   = "\\(".r  ^^ { _ => OPEN   }
-  val close  = "\\)".r  ^^ { _ => CLOSE  }
-  val let    = "let".r  ^^ { _ => LET    }
-  val equal  = "=".r    ^^ { _ => EQUAL  }
-  val in     = "in"     ^^ { _ => IN     }
+  val dot    = "."    ^^ { _ => DOT    }
+  val lambda = "\\"   ^^ { _ => LAMBDA }
+  val open   = "("    ^^ { _ => OPEN   }
+  val close  = ")"    ^^ { _ => CLOSE  }
+  val let    = "let " ^^ { _ => LET    }
+  val equal  = "="    ^^ { _ => EQUAL  }
+  val in     = "in "  ^^ { _ => IN     }
   val id     = new Regex("[a-zA-Z_$][a-zA-Z_$0-9]*") ^^ { id => ID(id) }
 
   val expr = rep1(dot | lambda | open | close | let | equal | in | id)
